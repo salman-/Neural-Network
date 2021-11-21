ae3d19654b414eed23bab7e430322ee46a087334
Salman Lashkarara <salman.lashkarara@mooncascade.com>
2021-11-21 13:46:22 +0100

    Update models

--- Insurance-Cost-Predictor/main.py
+++ Insurance-Cost-Predictor/main.py
@@ -1,10 +1,8 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Input
 from tensorflow.keras.layers import Dense
-
+import matplotlib.pyplot as plt
+import pandas as pd
 from Dataset import Dataset
 
 # The goal is to predict the insurance costs based on the other parameters
@@ -17,6 +15,7 @@ train_y = dataset.train_y
 
 test_x = dataset.process_data(dataset.test_x)
 test_y = dataset.test_y
+
 # ----------------------------------------
 # Create Neural-Network
 
@@ -41,14 +40,14 @@ insurance_model.compile(
 
 # ------------------------------------
 # Train the model
-callbacks = [tf.keras.callbacks.EarlyStopping(monitor='loss', patience=100),
+callbacks = [tf.keras.callbacks.EarlyStopping(monitor='loss', patience=5),
              tf.keras.callbacks.ModelCheckpoint(filepath='./insurance-predictor.h5')]
-history = insurance_model.fit(train_x, train_y, epochs=10000, callbacks=callbacks)
+history = insurance_model.fit(train_x, train_y, epochs=100, callbacks=callbacks)
 
 # ------------------------------------
 # Evaluate model on test dataset
 
-print(" Evaluate model on test: ", insurance_model.evaluate(test_x, test_y))
+#print(" Evaluate model on test: ", insurance_model.evaluate(test_x, test_y))
 
 pd.DataFrame(history.history).plot()
 plt.ylabel("MAE")
